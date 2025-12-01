47644451e79a1363c51bc962dbd059f86e0e2d62
tollkomji <bilostennijbogdan@gmail.com>
2025-12-01 10:05:00 +0200

    chore: init monorepo

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+const config = {
+  semi: true,
+  singleQuote: true,
+  printWidth: 100,
+  trailingComma: 'all',
+  tabWidth: 2,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+};
+
+export default config;
